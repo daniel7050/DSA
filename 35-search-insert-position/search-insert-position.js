5b0939fe936152efeb27f11be78392354144977c
Daniel Omole <danielomole18@gmail.com>
2026-04-14 17:03:20 +0100

    Time: 0 ms (100.00%) | Memory: 53.3 MB (84.12%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return mid; // Target found
+        } else if (nums[mid] < target) {
+            left = mid + 1; // Move to the right half
+        } else {
+            right = mid - 1; // Move to the left half
+        }
+    }
+
+    // If target is not found, return the insertion index
+    return left;
+
+};
